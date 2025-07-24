7bf36238e4a98e373c7f2dfacb66ebdc50fab4d0
ArchLeaders <archleadership28@gmail.com>
2025-07-24 11:00:38 -0700

    Support compressed outputs

--- TkScripts.LookupTables/Components/OutputStore.cs
+++ TkScripts.LookupTables/Components/OutputStore.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using TotkCommon;
 
 namespace TkScripts.LookupTables.Components;
 
@@ -10,7 +11,7 @@ public sealed class OutputStore(string outputFolder)
     
     private readonly string _outputFolder = outputFolder;
 
-    public async Task WriteResults(IGenerator generator, string[] gamePaths)
+    public async Task WriteResults(IGenerator generator, string[] gamePaths, bool compress = false)
     {
         Directory.CreateDirectory(_outputFolder);
         
@@ -29,6 +30,16 @@ public sealed class OutputStore(string outputFolder)
             Console.WriteLine($"Failed to serialize {typeName} as JSON: {e}");
         }
 
+        if (compress) {
+            await WriteCompressed(generator);
+            return;
+        }
+        
+        await WritePlain(generator);
+    }
+
+    private async Task WritePlain(IGenerator generator)
+    {
         foreach (object tag in generator.Tags) {
             string fileName = string.Format(generator.NameFormat, tag);
 
@@ -36,6 +47,23 @@ public sealed class OutputStore(string outputFolder)
             await using FileStream fs = File.Create(outputBinary);
             
             generator.WriteBinary(fs, tag);
+        }
+    }
+
+    private async Task WriteCompressed(IGenerator generator)
+    {
+        foreach (object tag in generator.Tags) {
+            string fileName = string.Format(generator.NameFormat, tag) + ".zs";
+
+            await using MemoryStream ms = new();
+            generator.WriteBinary(ms, tag);
+
+            if (!ms.TryGetBuffer(out ArraySegment<byte> buffer)) {
+                buffer = buffer.ToArray();
+            }
+            
+            string outputBinary = Path.Combine(_outputFolder, fileName);
+            File.WriteAllBytes(outputBinary, Zstd.Shared.Compress(buffer));
         }
     }
 }
